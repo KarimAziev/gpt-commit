14b9363f7dac73c0098c0427586a9d81585d8df5
KarimAziev <karim.aziiev@gmail.com>
2023-10-30 11:44:20 +0200

    style: change emoji for 'build' and 'revert' types
    - change emoji for 'build' and 'revert' types

--- gpt-commit.el
+++ gpt-commit.el
@@ -93,50 +93,51 @@ Can also be a function of no arguments that returns an API key (more secure)."
   :type 'string
   :group 'gpt-commit)
 
-(defcustom gpt-commit-types-alist '(("feat"
-                                     (description . "A new feature")
-                                     (title . "Features")
-                                     (emoji . "✨"))
-                                    ("fix"
-                                     (description . "A bug fix")
-                                     (title . "Bug Fixes")
-                                     (emoji . "🐛"))
-                                    ("docs"
-                                     (description . "Documentation only changes")
-                                     (title . "Documentation")
-                                     (emoji . "📚"))
-                                    ("style"
-                                     (description . "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)")
-                                     (title . "Styles")
-                                     (emoji . "💎"))
-                                    ("refactor"
-                                     (description . "A code change that neither fixes a bug nor adds a feature")
-                                     (title . "Code Refactoring")
-                                     (emoji . "📦"))
-                                    ("perf"
-                                     (description . "A code change that improves performance")
-                                     (title . "Performance Improvements")
-                                     (emoji . "🚀"))
-                                    ("test"
-                                     (description . "Adding missing tests or correcting existing tests")
-                                     (title . "Tests")
-                                     (emoji . "🚨"))
-                                    ("build"
-                                     (description . "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)")
-                                     (title . "Builds")
-                                     (emoji . "🛠"))
-                                    ("ci"
-                                     (description . "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)")
-                                     (title . "Continuous Integrations")
-                                     (emoji . "⚙️"))
-                                    ("chore"
-                                     (description . "Other changes that don't modify src or test files")
-                                     (title . "Chores")
-                                     (emoji . "♻️"))
-                                    ("revert"
-                                     (description . "Reverts a previous commit")
-                                     (title . "Reverts")
-                                     (emoji . "🗑")))
+(defcustom gpt-commit-types-alist
+  '(("feat"
+     (description . "A new feature")
+     (title . "Features")
+     (emoji . "✨"))
+    ("fix"
+     (description . "A bug fix")
+     (title . "Bug Fixes")
+     (emoji . "🐛"))
+    ("docs"
+     (description . "Documentation only changes")
+     (title . "Documentation")
+     (emoji . "📚"))
+    ("style"
+     (description . "Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)")
+     (title . "Styles")
+     (emoji . "💎"))
+    ("refactor"
+     (description . "A code change that neither fixes a bug nor adds a feature")
+     (title . "Code Refactoring")
+     (emoji . "📦"))
+    ("perf"
+     (description . "A code change that improves performance")
+     (title . "Performance Improvements")
+     (emoji . "🚀"))
+    ("test"
+     (description . "Adding missing tests or correcting existing tests")
+     (title . "Tests")
+     (emoji . "🚨"))
+    ("build"
+     (description . "Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)")
+     (title . "Builds")
+     (emoji . "🏗️"))
+    ("ci"
+     (description . "Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)")
+     (title . "Continuous Integrations")
+     (emoji . "⚙️"))
+    ("chore"
+     (description . "Other changes that don't modify src or test files")
+     (title . "Chores")
+     (emoji . "♻️"))
+    ("revert"
+     (description . "Reverts a previous commit")
+     (title . "Reverts")
+     (emoji . "↩️")))
   "A key-value-alist of commit types."
   :group 'gpt-commit
   :type '(alist
